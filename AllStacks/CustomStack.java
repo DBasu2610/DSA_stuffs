4272f7ea34dac9e7dc812420156aae1a0021da7c
DBasu2610 <basudebadrita94@gmail.com>
2024-07-11 11:58:46 +0530

    Stack added

---
+++ AllStacks/CustomStack.java
@@ -0,0 +1,55 @@
+package AllStacks;
+
+public class CustomStack {
+    protected int data[];
+    private static final int DefaultValue = 10;
+
+    int ptr = -1;
+
+    public CustomStack() {
+        this(DefaultValue);
+    }
+
+    public CustomStack(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean push(int item) {
+
+        if(isFull()) {
+            System.out.println("Stack is full");
+            return false;
+        }
+        ptr++;
+        data[ptr] = item;
+        return true;
+    }
+
+    public boolean isFull() {
+        return ptr == data.length-1;   
+    }
+
+    private boolean isEmpty() {
+        return ptr == -1;   
+    }
+
+    public int pop() {
+       if(isEmpty()) {
+        System.out.println("empty stack");
+       }
+       int item = data[ptr];
+       ptr--;
+       return item;
+    }
+
+    public int peek() {
+        if(isEmpty()) {
+            System.out.println("empty stack");
+           }
+        return data[ptr];
+    }
+
+
+    
+    
+}
---
+++ AllStacks/StackImplement.java
@@ -0,0 +1,22 @@
+package AllStacks;
+
+public class StackImplement {
+    public static void main(String[] args) {
+        CustomStack stack = new CustomStack(5);
+
+        stack.push(37);
+        stack.push(7);
+        stack.push(123);
+        stack.push(90);
+        stack.push(6);
+       
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+    }
+}
